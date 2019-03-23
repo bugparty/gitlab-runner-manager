501a10d1abac691fa799fe88a7c779f0d48684dd
ClayM0re <fancycode@gmail.com>
2019-03-23 21:50:48 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+url_x86_64 = `https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64`
+url_x86 = `https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-386`
+url_arm = `https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-arm`
+red='\e[91m'
+green='\e[92m'
+yellow='\e[93m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+
+# Root
+[[ $(id -u) != 0 ]] && echo -e "\n Please run as  ${red}root ${none} ${yellow}~(^_^) ${none}\n" && exit 1
+
+archs=`uname -m`
+url=`https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64'
+case "$archs" in
+    i?86) url=$url_x86 ;;
+    x86_64) url=$url_x86_64 ;;
+    arm) url=$url_arm ;;
+esac
+
+wget -O /usr/local/bin/gitlab-runner $url
+chmod +x /usr/local/bin/gitlab-runner
+useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+gitlab-runner start
+echo `type gitlab-runner register to registe your machine`
