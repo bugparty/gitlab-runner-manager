d1c5e8a1226cea505a5c260ed1fe23db70e32cde
ClayM0re <fancycode@gmail.com>
2019-03-24 23:32:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-url_x86_64 = `https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64`
-url_x86 = `https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-386`
-url_arm = `https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-arm`
+url_x86_64="https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64"
+url_x86="https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-386"
+url_arm="https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-arm"
 red='\e[91m'
 green='\e[92m'
 yellow='\e[93m'
@@ -14,16 +14,19 @@ none='\e[0m'
 [[ $(id -u) != 0 ]] && echo -e "\n Please run as  ${red}root ${none} ${yellow}~(^_^) ${none}\n" && exit 1
 
 archs=`uname -m`
-url=`https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64'
 case "$archs" in
-    i?86) url=$url_x86 ;;
-    x86_64) url=$url_x86_64 ;;
-    arm) url=$url_arm ;;
+    i?86) runner_url="$url_x86";;
+    x86_64) runner_url="$url_x86_64" ;;
+   arm) runner_url="$url_arm" ;;
 esac
+echo "download gitlab-runner $runner_url"
+if ! wget -O /usr/local/bin/gitlab-runner "$runner_url"; then
+   echo -e "$red download failed,exit! $none" && exit 1
+fi
 
-wget -O /usr/local/bin/gitlab-runner $url
 chmod +x /usr/local/bin/gitlab-runner
 useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
 gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
 gitlab-runner start
-echo `type gitlab-runner register to registe your machine`
+echo -e "$green install succeed! $none"
+echo 'type gitlab-runner register to registe your machine'
