d53c72b253a124df85cb7dc370fb4eb960076482
ClayM0re <fancycode@gmail.com>
2019-06-19 04:50:39 +0800

    add arm64 support

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 url_x86_64="https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64"
 url_x86="https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-386"
 url_arm="https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-arm"
+url_arm64="https://github.com/bugparty/gitlab-runner-manager/releases/download/gitlab-runner/gitlab-runner-linux-arm64"
 red='\e[91m'
 green='\e[92m'
 yellow='\e[93m'
@@ -18,7 +19,7 @@ case "$archs" in
     i?86) runner_url="$url_x86";;
     x86_64) runner_url="$url_x86_64" ;;
    arm) runner_url="$url_arm" ;;
-   aarch64) echo -e "$red armv8 is not supported $none" && exit 2 ;;
+   aarch64) runner_url="$url_arm64" ;;
 esac
 echo "download gitlab-runner $runner_url"
 if ! wget -O /usr/local/bin/gitlab-runner "$runner_url"; then
